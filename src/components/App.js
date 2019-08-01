9f4485fd022fa5ad4d21e7696412e7cf3b05148a
nikpalyi <nikolett.palyi@wipro.com>
2019-08-01 11:45:02 +0100

    comm

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import './App.css';
+
+import AgeStats from './AgeStats';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newDate: '',
+      birthday: '1985-08-05',
+      showStats: false //->ternary expression
+    };
+  }
+
+  changeBirthday() {
+    console.log(this.state);
+    this.setState({ birthday: this.state.newDate, showStats: true }); //important to set default state
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Form>
+          inline
+          <h2>Input Your Birthday!</h2>
+          <FormControl
+            type='date'
+            onChange={event => this.setState({ newDate: event.target.value })}
+          />
+          {''}
+          <Button onClick={() => this.changeBirthday()}>Submit</Button>
+          {this.state.showStats ? (
+            <div className='fade age-stats'>
+              <AgeStats date={this.state.birthday} />
+            </div>
+          ) : (
+            <div />
+          )}
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default App;
