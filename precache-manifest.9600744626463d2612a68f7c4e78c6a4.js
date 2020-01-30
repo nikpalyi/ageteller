0cc1280d26156b35badc2412a8922c78cb2617c8
Nikolett Palyi <nikpalyi@gmail.com>
2020-01-30 20:26:35 +0000

    Updates

---
+++ precache-manifest.9600744626463d2612a68f7c4e78c6a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187765fbac406db09798d04b10ee1c24",
+    "url": "/ageteller/index.html"
+  },
+  {
+    "revision": "e76101f9efcbcf44ea5f",
+    "url": "/ageteller/static/css/main.903e920c.chunk.css"
+  },
+  {
+    "revision": "004ff045f7730fc7e7d8",
+    "url": "/ageteller/static/js/2.8252c42e.chunk.js"
+  },
+  {
+    "revision": "e76101f9efcbcf44ea5f",
+    "url": "/ageteller/static/js/main.e03de04b.chunk.js"
+  },
+  {
+    "revision": "acc1b446f8c1308dd9c9",
+    "url": "/ageteller/static/js/runtime~main.fed48c24.js"
+  },
+  {
+    "revision": "d085efb4fdb109db221d8718f0dc04fc",
+    "url": "/ageteller/static/media/partyPopper.d085efb4.jpg"
+  }
+]);
